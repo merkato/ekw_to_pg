daa48ba98e0425d21fe0cc86a4bb9baf690d60a5
Tomasz Nycz <tomasz.merkato@gmail.com>
2021-10-27 08:20:37 +0200

    dodano znacznik czasu do opisu księgi, opisu położenia i opisu działki. Znacznik zapisywany w czasie importu

--- processing.py
+++ processing.py
@@ -1,8 +1,10 @@
 import xmltodict
 import collections
 import psycopg2
+import datetime as dt
 import glob
 import json
+
 
 def guaranteed_list(x):
     if not x:
@@ -12,34 +14,33 @@ def guaranteed_list(x):
     else:
         return [x]
 
-try:
-    conn = psycopg2.connect("dbname='osm' user='osm' host='localhost' password='osm'")
-except:
-    print("Blad polaczenia z baza")
-cur = conn.cursor()
-
-for filepath in glob.iglob('c:/ekw/*.xml'):
-    with open(filepath) as fd:
-       	doc = xmltodict.parse(fd.read())
+def numerksiegi(pole1):
     """ Przygotuj nowy numer księgi oraz opis stanu księgi (rubryka 0.2) """
-    wydzial = doc['KW']['R01']['P1']['@Wk']
-    repertorium = doc['KW']['R01']['P1']['@Nr']
-    cyfrakontroli = doc['KW']['R01']['P1']['@Ck']
-    kw = wydzial + '/'+ repertorium + '/' + cyfrakontroli
-    """ Przygotuj opis stanu księgi """
-    stankw = doc['KW']['R02']['P1']['@Tr']
-    zapisaniekw = doc['KW']['R02']['P2']['@Tr']
-    ujawnieniekw = doc['KW']['R02']['P3']['@Tr']
-    try:
-        dotychczasowakw = doc['KW']['R02']['P4']['@Tr']
-    except:
-        dotychczasowakw = 'brak'
-#Zapisz do bazy numer i stan księgi
-    cur.execute("INSERT INTO ekw.r02 VALUES(%s, %s, %s, %s, %s)", (kw, stankw, zapisaniekw, ujawnieniekw, dotychczasowakw))
+    wydzial = pole1['@Wk']
+    repertorium = pole1['@Nr']
+    cyfrakontroli = pole1['@Ck']
+    numerkw = wydzial + '/'+ repertorium + '/' + cyfrakontroli
+    return numerkw
 
+def stanksiegi(pole2):
+    opisksiegi= collections.OrderedDict()
+    """ Przygotuj opis stanu księgi """
+    opisksiegi['kw'] = numerksiegi(doc['KW']['R01']['P1'])
+    opisksiegi['stankw'] = pole2['P1']['@Tr']
+    opisksiegi['zapisaniekw'] = pole2['P2']['@Tr']
+    opisksiegi['ujawnieniekw'] = pole2['P3']['@Tr']
+    try:
+        opisksiegi['dotychczasowakw'] = pole2['P4']['@Tr']
+    except:
+        opisksiegi['dotychczasowakw'] = 'brak'
+    opisksiegi['znacznikczasu'] = dt.datetime.now()
+#Zapisz do bazy numer i stan księgi
+    return opisksiegi
+
+def polozenie(d1or13):
     """ Przygotuj rubrykę 1.3 w dziale I-O położenie nieruchomości"""
-    opispolozenia= collections.OrderedDict()
-    for polozenie in guaranteed_list(doc['KW']['D1o']['R13']['E']):
+    opispolozenia = collections.OrderedDict()
+    for polozenie in guaranteed_list(d1or13['E']):
         opispolozenia['kw'] = kw
         try:
             opispolozenia['lp'] = polozenie['P1']['@Tr']
@@ -61,11 +62,13 @@ for filepath in glob.iglob('c:/ekw/*.xml'):
             opispolozenia['miejscowosc'] = polozenie['P5']['@Tr']
         except:
             opispolozenia['miejscowosc'] = 'XX'
-        cur.execute("INSERT INTO ekw.d1or13 VALUES(%s, %s, %s, %s, %s, %s)", (list(opispolozenia.values())))
+        opispolozenia['znacznikczasu'] = dt.datetime.now()
+    return opispolozenia
 
+def dzialka(d1or14):
     """ Przygotuj rubrykę 1.4 podrubrykę 1.4.1 oznaczenie działki  """
     opisdzialki= collections.OrderedDict()
-    for oznaczenie in guaranteed_list(doc['KW']['D1o']['R14']['PR141']['E']):
+    for oznaczenie in guaranteed_list(d1or14['E']):
         opisdzialki['kw'] = kw
         try:
             opisdzialki['iddzialki'] = oznaczenie['P1']['@Tr']
@@ -90,21 +93,23 @@ for filepath in glob.iglob('c:/ekw/*.xml'):
             opisdzialki['lpn'] = oznaczenie['P4']['E']['@Tr']
         except:
             opisdzialki['lpn'] = 'nie wpisałem poprawnego - sprawdź'
-        cur.execute("INSERT INTO ekw.d1or14 VALUES(%s, %s, %s, %s, %s, %s)", (list(opisdzialki.values())))
+        opisdzialki['znacznikczasu'] = dt.datetime.now()
+    return opisdzialki
 
+def wlasciciel(d2r22):
     """ Przygotuj dane dla rubryki R2.2 księgi """
     try:
 # Tylko jedna rubryka PR2 E - szukamy w instytucjach jednego właściciela
         opiswlasciciela = collections.OrderedDict()
         opiswlasciciela['kw'] = kw
-        opiswlasciciela['zarzad'] = doc['KW']['D2']['R22']['PR2']['E']['SP']['I']['N']['@Tr']
+        opiswlasciciela['zarzad'] = d2r22['PR2']['E']['SP']['I']['N']['@Tr']
         opiswlasciciela['wlasciciel'] = 'sp - więcej wpisów'
         cur.execute("INSERT INTO ekw.d2r22 VALUES(%s, %s, %s)", (list(opiswlasciciela.values())))
     except:
         try:
 # Szukamy dwóch i więcej właścieili SP
             opiswlasciciela = collections.OrderedDict()
-            for skarb in doc['KW']['D2']['R22']['PR2']['E']['SP']['I']['N']:
+            for skarb in d2r22['PR2']['E']['SP']['I']['N']:
                 opiswlasciciela['kw'] = kw
                 opiswlasciciela['zarzad'] = skarb['@Tr']
                 opiswlasciciela['wlasciciel'] = 'sp - więcej wpisów'
@@ -113,7 +118,7 @@ for filepath in glob.iglob('c:/ekw/*.xml'):
             try:
 # Wiele rubryk PR2 E - szukamy w instytucjach
                 opiswlasciciela = collections.OrderedDict()
-                for skarb in doc['KW']['D2']['R22']['PR2']['E']:
+                for skarb in d2r22['PR2']['E']:
                     opiswlasciciela['kw'] = kw
                     opiswlasciciela['zarzad'] = skarb['SP']['I']['N']['@Tr']
                     opiswlasciciela['wlasciciel'] = 'sp - wpisy w wielu rubrykach'
@@ -122,7 +127,7 @@ for filepath in glob.iglob('c:/ekw/*.xml'):
                 try:
 # Rubryka PR3 E - szukamy w instytucjach JST
                     opiswlasciciela = collections.OrderedDict()
-                    for skarb in doc['KW']['D2']['R22']['PR3']['E']['JT']['I']['N']:
+                    for skarb in d2r22['PR3']['E']['JT']['I']['N']:
                         opiswlasciciela['kw'] = kw
                         opiswlasciciela['zarzad'] = skarb['@Tr']
                         opiswlasciciela['wlasciciel'] = 'jst'
@@ -130,12 +135,32 @@ for filepath in glob.iglob('c:/ekw/*.xml'):
                 except:
                     try:
 # Runryka PR5 - szukamy osób fizycznych
-                        zarzadnazwisko = doc['KW']['D2']['R22']['PR5']['E']['OF']['N1']['@Tr']
+                        zarzadnazwisko = d2r22['PR5']['E']['OF']['N1']['@Tr']
                         wlasnosc = 'NN - osoba fizyczna'
                         wlasciciel = 'of'
                         cur.execute("INSERT INTO ekw.d2r22 VALUES(%s, %s, %s)", (kw, wlasnosc, wlasciciel))
                     except:
                         print(kw + " - Nie odnalazłem poprawnie rubryk w R2.2")
+
+
+try:
+    conn = psycopg2.connect("dbname='osm' user='osm' host='localhost' password='osm'")
+except:
+    print("Blad polaczenia z baza")
+cur = conn.cursor()
+
+for filepath in glob.iglob('c:/ekw/*.xml'):
+    with open(filepath) as fd:
+        doc = xmltodict.parse(fd.read())
+        kw = numerksiegi(doc['KW']['R01']['P1'])
+        opisksiegi = stanksiegi(doc['KW']['R02'])
+        cur.execute("INSERT INTO ekw.r02 VALUES(%s, %s, %s, %s, %s, %s)", (list(opisksiegi.values())))
+        opispolozenia = polozenie(doc['KW']['D1o']['R13'])
+        cur.execute("INSERT INTO ekw.d1or13 VALUES(%s, %s, %s, %s, %s, %s, %s)", (list(opispolozenia.values())))
+        #opisdzialki = dzialka(doc['KW']['D1o']['R14']['PR141'])
+        #cur.execute("INSERT INTO ekw.d1or14 VALUES(%s, %s, %s, %s, %s, %s)", (list(opisdzialki.values())))
+        #wlasciciel(doc['KW']['D2']['R22'])
+
 conn.commit()
 cur.close()
 conn.close()
